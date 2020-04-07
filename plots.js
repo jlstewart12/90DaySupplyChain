9d7c7bfa5298d847d06de3fbedbfd2944f6dc0fb
jlstewart12 <57304171+jlstewart12@users.noreply.github.com>
2020-04-07 18:19:47 -0400

    initial commit

---
+++ plots.js
@@ -0,0 +1,88 @@
+/* global Plotly */
+var url =
+  `https://agtransport.usda.gov/resource/wnn7-29tu.json`;
+
+/**
+ * Helper function to select stock data
+ * Returns an array of values
+ * @param {array} rows
+ * @param {integer} index
+ * index 0 - Date
+ * index 1 - Open
+ * index 2 - High
+ * index 3 - Low
+ * index 4 - Close
+ * index 5 - Volume
+ */
+function unpack(rows, index) {
+  return rows.map(function(row) {
+    return row[index];
+  });
+}
+/**
+ * Fetch data and build the timeseries plot
+ */
+function buildPlot() {
+  // @TODO: YOUR CODE HERE
+  d3.json(url).then(function(data) {
+    var year = data.dataset.year;
+    var country = data.dataset.country;
+
+var trace1 = {
+  type: "line",
+  x: year,
+  y: country,
+};
+
+var data = [trace1];
+
+var layout = {
+  title: "test chart"
+}
+
+Plotly.newplot("mychart", data, layout);
+
+  });
+}
+
+buildPlot();
+
+
+
+// var ctx = document.getElementById('myChart');
+// var myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             }]
+//         }
+//     }
+// });
