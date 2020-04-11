f64883c8de031cd29cd62c1bbfafd649d0ebf479
jlstewart12 <57304171+jlstewart12@users.noreply.github.com>
2020-04-11 13:27:22 -0400

    newAppChanges

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template # For flask implementation    
+from bson.objectid import ObjectId # For ObjectId to work    
 import pymongo
-#import json
-#from bson.json_util import dumps
+import os
 
 app = Flask(__name__)
 
-conn = "mongodb://localhost:27017"
-client = pymongo.MongoClient(conn)
-
-db = client['exports_db']
-collection = db['collection']
+client = MongoClient("mongodb://127.0.0.1:27017") #host uri    
+db = client.mymongodb    #Select the database    
+exports_sales = db.collection #Select the collection name 
 
 # MONGODB_HOST = 'localhost'
 # MONGODB_PORT = 27017
@@ -17,12 +15,23 @@ collection = db['collection']
 # COLLECTION_NAME = 'collection'
 # FIELDS = {'Week':True,'Month':True,'Year':True,'Marketing Year':True,'Marketing Year':True,'Country':True,'Commodity':True,'Commodity Type':True,'Marketing Year Start or End':True,'Unit':True,'Weekly Exports CMY':True,'Accumulated Exports CMY':True,'Outstanding Sales CMY':True,'Gross Sales CMY':True, 'Net Sales CMY':True, 'Total Commitments CMY':True, 'Outstanding Sales NMY':True, 'Net Sales NMY':True, 'Marketing Month':True, 'Net Sales, Total':True, 'Outstanding Sales, Total':True,'_id':False}
 
+
+firstWeek = {'Week': '1'}
+soybeans = {'Commodity': 'Soybeans'}
+brazil = {'Country': 'Brazil'}
+
 @app.route("/")
 def index():
-    inventory = list(collection.find())
+    inventory = list(export_sales.find())
     print(inventory)
 
-    return render_template("index.html", inventory = inventory)
+    return render_template("index.html", holder1 = firstWeek, holder2 = soybeans, holder3 = brazil)
+
+#@app.route('/line')
+#def line():
+    #line_labels=labels
+    #line_values=values
+    #return render_template('line_chart.html', title='Bitcoin Monthly Price in USD', max=17000, labels=line_labels, values=line_values)
 
 # @app.route("/exports_db/collection")
 # def export_sales():
