60c991e30d4a069ed5ab3dedef12f5b72cabded0
Matt Houser <57680304+mhouser33@users.noreply.github.com>
2020-04-16 09:31:41 -0400

    Add files via upload

---
+++ Project 2/Chart 2/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request, render_template
+import pymongo
+import json
+import pandas as pd
+
+app = Flask(__name__)
+
+# setup mongo connection
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+# connect to mongo db and collection
+db = client.grainUSDA_db
+collection = db.collection
+
+@app.route("/")
+def index():
+    x = []
+    cursor = collection.find()
+    for i in cursor:
+        x.append(i)
+    # x = pd.DataFrame(list(db.grainUSDA.find())).to_dict()
+    # #
+    return render_template("index.html",Data=x)
+
+if __name__ == "__main__":
+    app.run(debug=True)
